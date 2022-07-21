579c66e90f59513038671fe2ac54f77ef8606544
arielsam567 <Saidemim112104>
2022-07-21 15:55:49 -0300

    update

---
+++ bin/backend.dart
@@ -0,0 +1,32 @@
+import 'package:shelf/shelf.dart';
+import 'apis/noticias_api.dart';
+import 'apis/login_api.dart';
+import 'apis/usuario_api.dart';
+import 'infra/custom_server.dart';
+import 'infra/dependency_injector/injects.dart';
+import 'infra/middleware_interception.dart';
+import 'utils/custom_env.dart';
+
+void main() async {
+  CustomEnv.fromFile('.env-dev');
+
+  final _di = Injects.initialize();
+
+  var cascadeHandler = Cascade()
+      .add(_di.get<LoginApi>().getHandler())
+      .add(_di.get<NoticiasApi>().getHandler(isSecurity: true))
+      .add(_di.get<UsuarioApi>().getHandler(isSecurity: true))
+      .handler;
+
+  var handler = Pipeline()
+      .addMiddleware(logRequests()) // global Middlewares
+      .addMiddleware(MInterception.contentTypeJson) // global Middlewares
+      .addMiddleware(MInterception.cors) // global Middlewares
+      .addHandler(cascadeHandler);
+
+  await CustomServer().initialize(
+    handler: handler,
+    address: await CustomEnv.get<String>(key: 'server_address'),
+    port: await CustomEnv.get<int>(key: 'server_port'),
+  );
+}
